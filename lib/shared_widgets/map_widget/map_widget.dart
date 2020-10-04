a3900147a599b040a16f62ac94c43365ddf3d515
Dushess0 <ilyabannila@gmail.com>
2020-10-04 18:04:18 +0200

    added map to map_widget.dart

--- lib/shared_widgets/map_widget/map_widget.dart
+++ lib/shared_widgets/map_widget/map_widget.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
+
+
 
 class MapWidget extends StatelessWidget {
+
+
+  static const String ACCESS_TOKEN = "SECRET_TOKEN";
+
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('It is map widget'),
+    return new Scaffold(
+        body: MapboxMap(
+          accessToken: ACCESS_TOKEN,
+          initialCameraPosition:
+          const CameraPosition(target: LatLng(0.0, 0.0)),
+        )
     );
   }
 }
